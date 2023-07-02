7623d4c4a32440056cc3314377e183d08808508e
InfoCQandP <134182813+InfoCQandP@users.noreply.github.com>
2023-07-02 14:09:43 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function irAInfo(url) {
+  window.location.href = url;
+  }
+
+  function mostrarMensaje() {
+    alert("¡Gracias por tu interés! Pronto nos pondremos en contacto contigo.");
+    location.reload();
+    }
